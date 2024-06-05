03f5a99af6b44495acfc93d77ea99ff8e8d462ce
Guillermo Raúl Domínguez Quintero <grdq2001@gmail.com>
2024-06-05 18:46:04 -0400

    Se agrega campo Location en clase Variable

--- Domain/Entities/ConfigurationData/Variable.cs
+++ Domain/Entities/ConfigurationData/Variable.cs
@@ -9,9 +9,12 @@ namespace Domain.Entities.ConfigurationData
     /// <summary>
     /// Clase dedicada a las variables medidas en cada habitación
     /// </summary>
-    public class Variable
+    public class Variable<T>
     {
-        
+        /// <summary>
+        /// Localización en la que es medida la variable
+        /// </summary>
+        public T Location { get; set; }
         /// <summary>
         /// Nombre de la variable
         /// </summary>
@@ -25,8 +28,9 @@ namespace Domain.Entities.ConfigurationData
         /// </summary>
         public string Code { get; set; }
 
-        public Variable(string name, string measurementUnit, string code)
+        public Variable(T location, string name, string measurementUnit, string code)
         {
+            Location = location;
             Name = name;
             MeasurementUnit = measurementUnit;
             Code = code;
