fe869013f05f09b7e0fcfc4d357a46f8ad5e0042
Meli <meli.liria02@gmail.com>
2024-06-05 18:49:54 -0400

    Se crea la clase Floor

---
+++ Domain/Entities/ConfigurationData/Floor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.ConfigurationData
+{
+    /// <summary>
+    /// Piso de un edificio
+    /// </summary>
+    public class Floor
+    {
+        #region Properties
+
+        /// <summary>
+        /// Ubicacion del  piso.
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Edificio donde se encuentra el piso
+        /// </summary>
+        public Building Building { get; set; }
+
+        #endregion 
+
+        /// <summary>
+        /// Inicializa un objeto <see cref="Floor"/>
+        /// </summary>
+        /// <param name="location">Ubicacion del piso.</param>
+        /// <param name="building">Edificio donde se encuentra el piso</param>
+        public Floor(string location,Building building)
+        {
+            Location = location;
+            Building = building;
+
+        }
+
+
+    }
+}
