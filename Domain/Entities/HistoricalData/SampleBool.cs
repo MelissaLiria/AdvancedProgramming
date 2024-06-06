35935ddb23bbfe393cbead2ba996ba7b05fe597f
Guillermo Raúl Domínguez Quintero <grdq2001@gmail.com>
2024-06-06 14:52:10 -0400

    Se crean 3 clases SampleBool, SampleDouble y SampleInt, hijas de Sample

---
+++ Domain/Entities/HistoricalData/SampleBool.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities.ConfigurationData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.HistoricalData
+{
+    public class SampleBool : Sample
+    {
+
+
+        /// <summary>
+        /// Valor booleano de la muestra
+        /// </summary>
+        public bool Value { get; set; }
+
+        /// <summary>
+        /// Inicializa un objeto tipo SampleBool
+        /// </summary>
+        /// <param name="variable"></param>
+        /// <param name="value"></param>
+        public SampleBool(Variable variable, bool value) : base(variable)
+        {
+            Value = value;
+        }
+
+    }
+}
