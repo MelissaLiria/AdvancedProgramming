4f419b50583f3bf592f3556138ce32079add793c
Guillermo Raúl Domínguez <grdq2001@gmail.com>
2024-06-18 18:15:11 -0400

    Creando SampleEntityTypeConfigurationBase

--- DataAccess/FluentConfigurations/Variables/VariableEntityTypeConfiguration.cs
+++ DataAccess/FluentConfigurations/Variables/VariableEntityTypeConfiguration.cs
@@ -15,10 +15,10 @@ namespace DataAccess.FluentConfigurations.Variables
         public override void Configure(EntityTypeBuilder<Variable> builder)
         {
             builder.ToTable("Variables");
-            base.Configure(builder);
             builder.Ignore(x => x.Location);
             builder.HasOne(x => x.Location).WithMany().HasForeignKey(x => x.LocationId);
             builder.OwnsOne(x => x.VariableType);
+            base.Configure(builder);
         }
     }
 }
