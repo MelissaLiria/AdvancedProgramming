ff80b25c5652b5568b31c6a9b412eb3779b779af
Guillermo Raúl Domínguez <grdq2001@gmail.com>
2024-06-18 17:57:29 -0400

    Se crea VariableEntityTypeConfiguration

---
+++ DataAccess/FluentConfigurations/Variables/VariableEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.FluentConfigurations.Common;
+using Domain.Entities.ConfigurationData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.FluentConfigurations.Variables
+{
+    public class VariableEntityTypeConfiguration : EntityTypeConfigurationBase<Variable>
+    {
+        public override void Configure(EntityTypeBuilder<Variable> builder)
+        {
+            builder.ToTable("Variables");
+            base.Configure(builder);
+            builder.Ignore(x => x.Location);
+            builder.HasOne(x => x.Location).WithMany().HasForeignKey(x => x.LocationId);
+            builder.OwnsOne(x => x.VariableType);
+        }
+    }
+}
