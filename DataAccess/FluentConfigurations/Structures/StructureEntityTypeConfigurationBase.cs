5fc0fe5f07e9baeb2018fe0a8b54a339dbedbf30
Meli <meli.liria02@gmail.com>
2024-06-18 18:42:21 -0400

    Se crea FloorEntityTypeConfiguration

---
+++ DataAccess/FluentConfigurations/Structures/StructureEntityTypeConfigurationBase.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.FluentConfigurations.Common;
+using Domain.Entities.ConfigurationData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.FluentConfigurations.Structures
+{
+    public class StructureEntityTypeConfigurationBase
+        :EntityTypeConfigurationBase<Structure>
+    {
+        public override void Configure(EntityTypeBuilder<Structure> builder)
+        {
+            builder.ToTable("Structure");
+            base.Configure(builder);
+        }
+    }
+}
