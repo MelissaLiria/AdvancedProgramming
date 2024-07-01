bd3e293599874d9fbe807f28e929e9b9ddcf4070
Meli <meli.liria02@gmail.com>
2024-07-01 17:41:44 -0400

    Implementando StructureRepository

--- DataAccess/Repositories/Structures/StructureRepository.cs
+++ DataAccess/Repositories/Structures/StructureRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Contracts.Structures;
+using DataAccess.Contexts;
+using Domain.Entities.ConfigurationData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,43 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Repositories.Structures
 {
-    internal class StructureRepository
+    /// <summary>
+    /// Implementación del repositorio IStructureRepository
+    /// </summary>
+    public class StructureRepository
+        : RepositoryBase, IStructureRepository
     {
-    }
+        public StructureRepository(ApplicationContext context) : base(context)
+        {
+        }
+
+        public void AddStructure(Structure structure)
+        {
+            _context.Structures.Add(structure);
+        }
+
+        public void UpdateStructure(Structure structure)
+        {
+            _context.Structures.Update(structure);
+        }
+
+        public void DeleteStructure(Structure structure)
+        {
+            _context.Structures.Remove(structure);
+        }
+
+        public T? GetStructureById<T>(Guid id) where T : Structure
+        {
+            return _context.Set<T>().FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<T> GetAllStructures<T>() where T : Structure
+        {
+            return _context.Set<T>().ToList();
+        }
+
+       
+
+       
+      }
 }
