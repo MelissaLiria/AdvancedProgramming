c4012d20a062d2644e8dd33c85e7bf5d066be715
Meli <meli.liria02@gmail.com>
2024-07-02 00:14:40 -0400

    Arreglando namespace

--- DataAccess/Repositories/Common/RepositoryBase.cs
+++ DataAccess/Repositories/Common/RepositoryBase.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Repositories
+namespace DataAccess.Repositories.Common
 {
     /// <summary>
     /// Clase base para repositorios.
